08e23163fd7e65bd035a594200b8eea01e60e528
fernandofernandes-code <fernando.fernandes@gmail.com>
2026-07-10 16:26:50 -0300

    Replace hardcoded credentials with environment variables

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-const API_KEY = "ghp_xK92mNpL34rTvQ87wZaB56cDeFgHiJkL";
-const DB_PASSWORD = "admin@prod#2024";
+const API_KEY = process.env.API_KEY;;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 // Busca tarefas do "banco de dados"
 fetch('db.json')
